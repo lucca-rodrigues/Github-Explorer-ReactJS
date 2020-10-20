f3130d2286368a63dfcc31e7e01ce54c372399f0
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2020-10-20 18:34:12 -0300

    Page Repository details

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, FormEvent }from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import {Link} from 'react-router-dom'
 
 import api from '../../Services/api';
 
@@ -75,14 +76,14 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map(repository => (
-          <a target="_blank" rel="noopener noreferrer" key="repository.full_name" href={repository.owner.avatar_url}>
+          <Link key="repository.full_name" to={`/repositories/${repository.full_name}`}>
           <img src={repository.owner.avatar_url} alt={repository.full_name}/>
           <div>
             <strong>{repository.full_name}</strong>
             <p>{repository.description}</p>
           </div>
           <FiChevronRight size={20}/>
-        </a>
+        </Link>
         ))}
       </Repositories>
     </>
